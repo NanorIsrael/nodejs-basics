c8890fda108b77ff26aeaddf3f0ec03e84f8d0d4
nanorisrael <israel.nanor@compozelabs.com>
2024-02-27 11:39:28 +0000

    Add http server

---
+++ 4-http.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.contentType = 'plain/text';
+  res.end('Hello GVTech School! ');
+});
+app.listen(1245, "localhost", () => null);
